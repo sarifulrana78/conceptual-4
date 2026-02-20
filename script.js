fbf9664ac34b8ba4107dcf263773113feb2aab63
Rana Bhai <sarifulislam536@gmail.com>
2026-02-20 21:55:35 +0600

    connent html-css-js succesfully

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-console.log("hello")
+/* console.log("hello")
 //const mainText= document.getElementById("mainTitle")
 
 //for id:
 //const mainText= document.querySelector("#mainTitle")
 // for class:
-const listItems= document.querySelectorAll(".fruits")
+const listItems= document.querySelectorAll(".fruits") */
 //console.log(mainText.innerText = "Hello dev")
-console.log(listItems[0])
+//console.log(listItems[0])
+//const mainDiv =document.querySelector("#mainDiv")
+//console.log(mainDiv.children[1])
+//const img=document.querySelector("img")
+//console.log(img)
+//img.setAttribute("alt" , "updated.jpg")
+const listItems =document.getElementsByClassName("list-Item")
+//console.log(listItems[0])
+listItems[0].style.color="red"
