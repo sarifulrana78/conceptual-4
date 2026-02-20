07985dcd8ea3736b16115ec9e74bce330df3fa6c
Rana Bhai <sarifulislam536@gmail.com>
2026-02-20 07:35:48 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("hello")
+//const mainText= document.getElementById("mainTitle")
+
+//for id:
+//const mainText= document.querySelector("#mainTitle")
+// for class:
+const listItems= document.querySelectorAll(".fruits")
+//console.log(mainText.innerText = "Hello dev")
+console.log(listItems[0])
